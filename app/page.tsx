2d4c86af4bbc476524255bcc4539be7c3d1d3c3b
shiwoo.park <custardcream@kakao.com>
2023-12-11 02:33:40 +0900

    feat(MainPage): 데이터 연결

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,35 @@
+import { ServiceSearchForm } from "@/components/ServiceSearchForm.server";
 import AuthButton from "../components/AuthButton";
+import { OpenDataViewer } from "@/components/OpenDataViewer.server";
+import { OpenDataSampleViewer } from "@/components/OpenDataSampleViewer.server";
+import { ErrorMessageToast } from "@/components/ErrorMessageToast";
+import type { GeoFieldType } from "@/types/field";
 
-export default async function Index() {
+type AddressSearchParams = {
+  fieldType: "address";
+  addressFieldName: string;
+};
+
+type CoordinateSearchParams = {
+  fieldType: "coordinate";
+  latitudeFieldName: string;
+  longitudeFieldName: string;
+};
+
+type MainPageSearchParams = {
+  serviceName?: string;
+  errorId?: string;
+} & (
+  | { fieldType: undefined }
+  | AddressSearchParams
+  | CoordinateSearchParams
+);
+
+export default async function MainPage({
+  searchParams,
+}: {
+  searchParams?: MainPageSearchParams;
+}) {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
@@ -8,6 +37,34 @@ export default async function Index() {
           <AuthButton />
         </div>
       </nav>
+      <ServiceSearchForm />
+      {searchParams && (
+        <>
+          {searchParams.serviceName && (
+            <OpenDataSampleViewer
+              serviceName={searchParams.serviceName}
+            />
+          )}
+          {searchParams.fieldType === "address" && (
+            <div>
+              FieldType Address:{" "}
+              {searchParams.addressFieldName}
+            </div>
+          )}
+          {searchParams.fieldType === "coordinate" && (
+            <div>
+              FieldType Coordinate:{" "}
+              {searchParams.latitudeFieldName},{" "}
+              {searchParams.longitudeFieldName}
+            </div>
+          )}
+          {searchParams.errorId && (
+            <ErrorMessageToast
+              errorId={searchParams.errorId}
+            />
+          )}
+        </>
+      )}
     </div>
   );
 }
