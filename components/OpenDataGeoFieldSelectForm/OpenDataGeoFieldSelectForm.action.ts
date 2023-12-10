cdac894cc919d380a4d99305164e592dcc8f2806
shiwoo.park <custardcream@kakao.com>
2023-12-11 02:32:07 +0900

    feat(OpenDataGeoFieldSelectForm): 좌표 선택 폼 컴포넌트

---
+++ components/OpenDataGeoFieldSelectForm/OpenDataGeoFieldSelectForm.action.ts
@@ -0,0 +1,146 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { ErrorId } from "../ErrorMessageToast/ErrorMessageToast.type";
+import type { GeoFieldType } from "@/types/field";
+
+const isString = (value: unknown): value is string =>
+  typeof value === "string";
+
+const isValidLongitude = (value: number) =>
+  value >= -180 && value <= 180;
+
+const isValidLatitude = (value: number) =>
+  value >= -90 && value <= 90;
+
+const redirectWithErrorId = (
+  serviceName: string,
+  errorId: ErrorId
+) =>
+  redirect(
+    `/?${new URLSearchParams({
+      serviceName,
+      errorId: errorId.toString(),
+    })}`
+  );
+
+function redirectWithValidField(
+  serviceName: string,
+  fieldType: "address",
+  addressFieldName: string
+): void;
+function redirectWithValidField(
+  serviceName: string,
+  fieldType: "coordinate",
+  longitudeFieldName: string,
+  latitudeFieldName: string
+): void;
+function redirectWithValidField(
+  serviceName: string,
+  fieldType: GeoFieldType,
+  ...fieldNames: string[]
+) {
+  if (fieldType === "address") {
+    const [addressFieldName] = fieldNames;
+
+    return redirect(
+      `/?${new URLSearchParams({
+        serviceName,
+        fieldType,
+        addressFieldName,
+      })}`
+    );
+  } else {
+    const [longitudeFieldName, latitudeFieldName] =
+      fieldNames;
+
+    return redirect(
+      `/?${new URLSearchParams({
+        serviceName,
+        fieldType,
+        longitudeFieldName,
+        latitudeFieldName,
+      })}`
+    );
+  }
+}
+
+export const redirectToFetchData = async (
+  sampleDataForValidation: Record<string, unknown>,
+  serviceName: string,
+  formData: FormData
+) => {
+  const addressField = formData.get("addressField");
+  const longitudeField = formData.get("longitudeField");
+  const latitudeField = formData.get("latitudeField");
+
+  if (isString(addressField)) {
+    const isValid =
+      typeof sampleDataForValidation[addressField] ===
+      "string";
+
+    if (!isValid) {
+      return redirectWithErrorId(
+        serviceName,
+        ErrorId.InvalidAddressField
+      );
+    }
+
+    return redirectWithValidField(
+      serviceName,
+      "address",
+      addressField
+    );
+  }
+
+  if (longitudeField === latitudeField) {
+    return redirectWithErrorId(
+      serviceName,
+      ErrorId.SameCoordinateFieldNames
+    );
+  }
+
+  const sampleLongitude =
+    isString(longitudeField) &&
+    sampleDataForValidation[longitudeField];
+  const sampleLatitude =
+    isString(latitudeField) &&
+    sampleDataForValidation[latitudeField];
+
+  const parsedLongitude = Number(sampleLongitude);
+  const parsedLatitude = Number(sampleLatitude);
+
+  const isValidLongitudeField =
+    sampleLongitude && isValidLongitude(parsedLongitude);
+
+  const isValidLatitudeField =
+    sampleLatitude && isValidLatitude(parsedLatitude);
+
+  if (!isValidLongitudeField && !isValidLatitudeField) {
+    return redirectWithErrorId(
+      serviceName,
+      ErrorId.InvalidCoordinateFields
+    );
+  }
+
+  if (!isValidLongitudeField) {
+    return redirectWithErrorId(
+      serviceName,
+      ErrorId.InvalidLongitudeField
+    );
+  }
+
+  if (!isValidLatitudeField) {
+    return redirectWithErrorId(
+      serviceName,
+      ErrorId.InvalidLatitudeField
+    );
+  }
+
+  return redirectWithValidField(
+    serviceName,
+    "coordinate",
+    longitudeField,
+    latitudeField
+  );
+};
---
+++ components/OpenDataGeoFieldSelectForm/OpenDataGeoFieldSelectForm.client.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import { useMemo, useState } from "react";
+import { redirectToFetchData } from "./OpenDataGeoFieldSelectForm.action";
+
+enum SelectingType {
+  Type,
+  Address,
+  Coordinate,
+}
+
+export const OpenDataGeoFieldSelectForm = ({
+  serviceName,
+  data,
+}: {
+  serviceName: string;
+  data: Record<string, unknown>;
+}) => {
+  const [selectingType, setSelectingType] =
+    useState<SelectingType>(SelectingType.Type);
+
+  const options = useMemo(
+    () =>
+      Object.entries(data).map(([key, value]) => (
+        <option key={key} value={key}>
+          '{key}', 샘플 데이터: {JSON.stringify(value)}
+        </option>
+      )),
+    [data]
+  );
+
+  const redirectToFetchDataWithSampleData =
+    redirectToFetchData.bind(null, data, serviceName);
+
+  return (
+    <div>
+      <fieldset className="mb-2">
+        <legend className="mb-1">
+          주소 / 좌표중 어떤 필드를 선택할지 결정해주세요.
+        </legend>
+        <label>
+          <input
+            className="mr-2"
+            type="radio"
+            name="data-type"
+            value="address"
+            onChange={() =>
+              setSelectingType(SelectingType.Address)
+            }
+          />
+          주소 필드 선택하기
+        </label>
+        <label className="ml-4">
+          <input
+            className="mr-2"
+            type="radio"
+            name="data-type"
+            value="coordinate"
+            onChange={() =>
+              setSelectingType(SelectingType.Coordinate)
+            }
+          />
+          좌표 필드 선택하기
+        </label>
+      </fieldset>
+      {selectingType !== SelectingType.Type && (
+        <form action={redirectToFetchDataWithSampleData}>
+          <div className="flex flex-col gap-2">
+            {selectingType === SelectingType.Address && (
+              <>
+                <label htmlFor="addressField">
+                  주소 필드 선택
+                </label>
+                <select
+                  name="addressField"
+                  id="addressField"
+                  className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+                  defaultValue={Object.keys(data)[0]}
+                >
+                  {options}
+                </select>
+              </>
+            )}
+            {selectingType === SelectingType.Coordinate && (
+              <>
+                <label htmlFor="latitudeField">
+                  위도 필드 선택
+                </label>
+                <select
+                  name="latitudeField"
+                  id="latitudeField"
+                  className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+                  defaultValue={Object.keys(data)[0]}
+                >
+                  {options}
+                </select>
+                <label htmlFor="longitudeField">
+                  경도 필드 선택
+                </label>
+                <select
+                  name="longitudeField"
+                  id="longitudeField"
+                  className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+                  defaultValue={Object.keys(data)[0]}
+                >
+                  {options}
+                </select>
+              </>
+            )}
+          </div>
+          <button>데이터 불러오기</button>
+        </form>
+      )}
+    </div>
+  );
+};
