5ac43f7ef46b2a25526e92c194f063d1983188f0
shiwoo.park <custardcream@kakao.com>
2023-12-13 05:36:03 +0900

    feat(crypto): encode, decode Base64로 개선

---
+++ utils/crypto.ts
@@ -0,0 +1,17 @@
+import CryptoJs from "crypto-js";
+
+export const encode = (target: string) => {
+  const wordArray = CryptoJs.enc.Utf16.parse(target);
+  const base64 = CryptoJs.enc.Base64.stringify(wordArray);
+
+  return base64;
+};
+
+export const decode = (target: string) => {
+  const parsedWordArray = CryptoJs.enc.Base64.parse(target);
+  const parsedStr = parsedWordArray.toString(
+    CryptoJs.enc.Utf16
+  );
+
+  return parsedStr;
+};
