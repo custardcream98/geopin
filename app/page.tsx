59ab6d51b558ce912fb03bbdbdace3419b97a920
shiwoo.park <custardcream@kakao.com>
2023-12-13 04:50:17 +0900

    chore(auth): 임시로 auth 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { ServiceSearchForm } from "@/components/ServiceSearchForm.server";
-import AuthButton from "../components/AuthButton";
+// import { ServiceSearchForm } from "@/components/ServiceSearchForm.server";
+// import AuthButton from "../components/AuthButton";
 import { OpenDataViewer } from "@/components/OpenDataViewer.server";
 import { OpenDataSampleViewer } from "@/components/OpenDataSampleViewer.server";
 import { ErrorMessageToast } from "@/components/ErrorMessageToast";
 import { GeoFieldType } from "@/types/field";
 import { OpenDataTitleViewer } from "@/components/OpenDataTitleViewer.server";
-import { getAuth } from "@/utils/supabase/server";
+// import { getAuth } from "@/utils/supabase/server";
 
 type AddressSearchParams = {
   fieldType: "address";
@@ -39,12 +39,12 @@ export default async function MainPage({
 }: {
   searchParams?: MainPageSearchParams;
 }) {
-  const auth = await getAuth();
-  const isLogged = auth?.user !== null;
+  // const auth = await getAuth();
+  // const isLogged = auth?.user !== null;
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b h-16">
+      {/* <nav className="w-full flex justify-center border-b h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           <AuthButton />
         </div>
@@ -53,44 +53,48 @@ export default async function MainPage({
         <ServiceSearchForm />
       ) : (
         <div>로그인이 필요합니다.</div>
-      )}
-      {isLogged && searchParams && (
-        <>
-          {searchParams.serviceNameKorean && (
-            <OpenDataTitleViewer>
-              {searchParams.serviceNameKorean}
-            </OpenDataTitleViewer>
-          )}
-          {searchParams.serviceName &&
-            searchParams.epsg && (
-              <>
-                <OpenDataSampleViewer
-                  searchParams={{
-                    ...searchParams,
-                    epsg: searchParams.epsg,
-                    serviceName: searchParams.serviceName,
-                  }}
-                />
-                {(searchParams.fieldType === "address" ||
-                  searchParams.fieldType ===
-                    "coordinate") && (
-                  <OpenDataViewer
+      )} */}
+      {
+        // isLogged &&
+        searchParams && (
+          <>
+            {searchParams.serviceNameKorean && (
+              <OpenDataTitleViewer>
+                {searchParams.serviceNameKorean}
+              </OpenDataTitleViewer>
+            )}
+            {searchParams.serviceName &&
+              searchParams.epsg && (
+                <>
+                  <OpenDataSampleViewer
                     searchParams={{
                       ...searchParams,
                       epsg: searchParams.epsg,
                       serviceName: searchParams.serviceName,
                     }}
                   />
-                )}
-              </>
+                  {(searchParams.fieldType === "address" ||
+                    searchParams.fieldType ===
+                      "coordinate") && (
+                    <OpenDataViewer
+                      searchParams={{
+                        ...searchParams,
+                        epsg: searchParams.epsg,
+                        serviceName:
+                          searchParams.serviceName,
+                      }}
+                    />
+                  )}
+                </>
+              )}
+            {searchParams.errorId && (
+              <ErrorMessageToast
+                errorId={searchParams.errorId}
+              />
             )}
-          {searchParams.errorId && (
-            <ErrorMessageToast
-              errorId={searchParams.errorId}
-            />
-          )}
-        </>
-      )}
+          </>
+        )
+      }
     </div>
   );
 }
