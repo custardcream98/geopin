42a36e2c8b52e2b0c5f9ea930058f8f1544652db
shiwoo.park <custardcream@kakao.com>
2023-12-17 14:39:45 +0900

    feat(crypto): url friendly하게 수정

--- utils/crypto.ts
+++ utils/crypto.ts
@@ -2,13 +2,15 @@ import CryptoJs from "crypto-js";
 
 export const encode = (target: string) => {
   const wordArray = CryptoJs.enc.Utf16.parse(target);
-  const base64 = CryptoJs.enc.Base64.stringify(wordArray);
+  const base64 =
+    CryptoJs.enc.Base64url.stringify(wordArray);
 
   return base64;
 };
 
 export const decode = (target: string) => {
-  const parsedWordArray = CryptoJs.enc.Base64.parse(target);
+  const parsedWordArray =
+    CryptoJs.enc.Base64url.parse(target);
   const parsedStr = parsedWordArray.toString(
     CryptoJs.enc.Utf16
   );
